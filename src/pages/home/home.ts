42d73fc02a5df0ae0e8e801c69476ad3a47c64f9
GILBERT LUGO <glpunk@gmail.com>
2017-01-13 18:29:43 -0300

    ajustes home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -34,27 +34,6 @@ export class HomePage {
   }
 
   refresh(){
-    this.favorites = [];
-
-    this.db.getFavorites().then((data) => {
-      console.log('fav data', data);
-      if(data.rows.length > 0) {
-        for(var i = 0; i < data.rows.length; i++) {
-          let time_str = this.utils.formatTime( (data.rows.item(i).minutes*60) + data.rows.item(i).seconds );
-          this.favorites.push({
-            id: data.rows.item(i).id, 
-            name: data.rows.item(i).name, 
-            description: data.rows.item(i).description, 
-            time: time_str,
-            img: data.rows.item(i).img,
-            favorite: data.rows.item(i).favorite,
-          });
-        }
-      }
-    }, (error) => {
-      let scope = this;
-      setTimeout(function(){ scope.refresh(); },100);
-    });
 
     this.activities = [];
 
@@ -70,6 +49,27 @@ export class HomePage {
             time: time_str,
             img: data.rows.item(i).img,
             when: data.rows.item(i).createdAt
+          });
+        }
+      }
+    }, (error) => {
+    });
+
+
+    this.favorites = [];
+
+    this.db.getFavorites().then((data) => {
+      console.log('fav data', data);
+      if(data.rows.length > 0) {
+        for(var i = 0; i < data.rows.length; i++) {
+          let time_str = this.utils.formatTime( (data.rows.item(i).minutes*60) + data.rows.item(i).seconds );
+          this.favorites.push({
+            id: data.rows.item(i).id, 
+            name: data.rows.item(i).name, 
+            description: data.rows.item(i).description, 
+            time: time_str,
+            img: data.rows.item(i).img,
+            favorite: data.rows.item(i).favorite,
           });
         }
       }
